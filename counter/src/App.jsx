de8ef0100bb0bde78398d620f88bad11bf1342f2
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2025-04-04 17:11:15 +0000

    Counter Project

---
+++ counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ let [counter,setCounter]=useState(15)
+
+
+
+// let Counter=5
+const addValue=()=>{
+  // counter+=1
+  setCounter(counter+1)
+  if(counter===20){
+    setCounter(counter)
+  }
+}
+
+const removeValue=()=>{
+  // counter+=1
+  
+  setCounter(counter-1)
+  if(counter===0){
+    setCounter(counter)
+  }
+}
+
+
+
+  return (
+    <>
+  <h1>Chai aur React</h1>
+  <h2>Counter value:{counter}</h2>
+
+  <button onClick={addValue}>Add Value </button>
+  <br/>
+  <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
+
+
+// hme updation k liye HOOKS ka use karna hota hai 
